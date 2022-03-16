7d96f0a4e1efa15bbf3c208c040465975fd28f85
NIPHAM53 <nipham53@gmail.com>
2022-03-16 02:57:07 -0400

    Added abilty for user input

--- src/CountingHeadsOrTails.java
+++ src/CountingHeadsOrTails.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class CountingHeadsOrTails {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("Guess which will have more: Heads or Tails?");
+	Scanner scnr = new Scanner(System.in); 
+	String headsorTailsGuess = scnr.nextLine();
+	System.out.println("How many times shall we flip the coin?");
+	int numberOfFlips = scnr.nextInt();
+	int random = (int)(Math.random()*2);
 	}
 
 }
